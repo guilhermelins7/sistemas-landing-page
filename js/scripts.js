81ef53fd0c9ab865216c1d66bb5aba2a287926dc
guilhermelins7 <guilherme7lins@gmail.com>
2024-12-21 14:55:05 -0300

    feat: criado section grade

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// script grade
+
+function toggleList(element) {
+    const content = element.nextElementSibling; // Seleciona próximo elemento no DOM
+
+    if (content.style.display === 'none' || content.style.display === '')
+        content.style.display = 'block';
+    else content.style.display = 'none';
+}
+
+function toggleIcon(id) {
+    const toggle = document.getElementById(id);
+
+    if (toggle.textContent === '+') toggle.textContent = '-';
+    else toggle.textContent = '+';
+}
+
+// script grade/
